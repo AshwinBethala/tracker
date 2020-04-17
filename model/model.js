9a9c32d93a9d30c74dde36cfc3386f717274458d
Ashwin Bethala <57239378+AshwinBethala@users.noreply.github.com>
2020-04-16 23:57:13 -0500

    Add files via upload

---
+++ model/model.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+
+    {
+        day: {
+            type: Date,
+            required: true,
+        },
+
+        excerises: {
+            
+            type: {
+                type: String,
+                required: true,
+                },
+
+            name: {
+                type: String,
+                required: true,
+            },
+                
+            duration: {
+                type: Number,
+                required: false,
+            },
+                
+            weight: {
+                type: Number,
+                required: false,
+            },
+                
+            reps: {
+                type: Number,
+                required: false,
+            },
+                
+            sets: {
+                type: Number,
+                required: false,
+            },
+        }
+    }
+
+)
+
+module.exports = mongoose.model("Workout", workoutSchema)
