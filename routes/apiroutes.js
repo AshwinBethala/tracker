1e71706eae687c70b686c0999ff6ba71bc6b7e5b
Ashwin Bethala <57239378+AshwinBethala@users.noreply.github.com>
2020-05-30 15:05:59 -0500

    Update apiroutes.js

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,8 +1,38 @@
-const Workout = require ("model/model.js")
+const Workout = require ("../model/model.js")
 
 module.exports = function (app) {
 
     app.get("public/api.js", function (req, res) {
+        Workout.find()
+            .then(data => {
+                res.json(data)
+            })
+            .catch(err => {
+                res.json(err)
+            })
+    });
+
+    app.get("public/exercise.js", function (req, res) {
+        Workout.find()
+            .then(data => {
+                res.json(data)
+            })
+            .catch(err => {
+                res.json(err)
+            })
+    });
+
+    app.get("public/index.js", function (req, res) {
+        Workout.find()
+            .then(data => {
+                res.json(data)
+            })
+            .catch(err => {
+                res.json(err)
+            })
+    });
+
+    app.get("public/stats.js", function (req, res) {
         Workout.find()
             .then(data => {
                 res.json(data)
