48f8fa8ba094c779be687c713e5ccba05a6fba25
Ashwin Bethala <57239378+AshwinBethala@users.noreply.github.com>
2020-06-01 09:10:33 -0500

    Update apiroutes.js

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,9 +1,37 @@
 const Workout = require ("../model/model.js")
-
+​
 module.exports = function (app) {
-
-    app.get("public/api.js", function (req, res) {
+​
+    app.get("/api/workouts", (req, res) => {
         Workout.find()
+          .then(data => {
+            res.json(data);
+          })
+          .catch(err => {
+            res.json(err);
+          });
+    });
+​
+    app.put("/api/workouts/:id", function (req, res) {
+​
+        const {id, body} = req.params;
+​
+        Workout.findByIdAndUpdate(
+            id, 
+            { $push: { exercises: body } },
+            { new: true}) // Return new updated item as the dbWorkout result
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+​
+    app.post("/api/workouts", function (req, res) {
+​
+        /* TODO complete create */
+        Workout.create()
             .then(data => {
                 res.json(data)
             })
@@ -11,9 +39,11 @@ module.exports = function (app) {
                 res.json(err)
             })
     });
-
-    app.get("public/exercise.js", function (req, res) {
-        Workout.find()
+​
+    app.get("/api/workouts/range", function (req, res) {
+​
+        /* TODO complete limit */
+        Workout.limit()
             .then(data => {
                 res.json(data)
             })
@@ -21,25 +51,5 @@ module.exports = function (app) {
                 res.json(err)
             })
     });
-
-    app.get("public/index.js", function (req, res) {
-        Workout.find()
-            .then(data => {
-                res.json(data)
-            })
-            .catch(err => {
-                res.json(err)
-            })
-    });
-
-    app.get("public/stats.js", function (req, res) {
-        Workout.find()
-            .then(data => {
-                res.json(data)
-            })
-            .catch(err => {
-                res.json(err)
-            })
-    });
-
+​
 }
